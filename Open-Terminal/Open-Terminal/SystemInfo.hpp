4a6a0acc67ac7e2561b748c3e78e059f4e84790f
Vlad <297415871+JomirVladimir@users.noreply.github.com>
2026-06-28 13:59:48 +0300

    Alfa version

--- Open-Terminal/Open-Terminal/SystemInfo.hpp
+++ Open-Terminal/Open-Terminal/SystemInfo.hpp
@@ -9,17 +9,10 @@ std::string Systeminfo() {
 	SYSTEM_POWER_STATUS baterie; // Variabil "baterie" în care se stochează procenrul de baterie
 	SetConsoleOutputCP(CP_UTF8); // Setarea limbei română în terminal
 	SetConsoleCP(CP_UTF8);       //
-	while (comand != "exit") {
-		if (GetSystemPowerStatus(&baterie)) {																				 //
-			if (baterie.BatteryLifePercent != 225) {																		 //
-				std::cout << "Procet de baterie: " << (int)baterie.BatteryLifePercent << "%" << std::endl; // Afișează procentul de baterie
-			}																															 //
-		}																																 //
-		std::cout << "Terminal\\SI-> ";
-		std::cin >> comand; // Citorea comandelor de la utilizatorul
-		if (comand == "fm") {
-			return "fm";
-		}
-	}
-	return "end";
+	if (GetSystemPowerStatus(&baterie)) {																				 //
+		if (baterie.BatteryLifePercent != 225) {																		 //
+			std::cout << "Procet de baterie: " << (int)baterie.BatteryLifePercent << "%" << std::endl; // Afișează procentul de baterie
+		}																															 //
+	}																																 //
+	
 }
