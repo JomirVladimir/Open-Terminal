c3ca9749d966f2fcec3ae1eda10d29bbcbd9ce88
Vlad <297415871+JomirVladimir@users.noreply.github.com>
2026-06-28 14:04:47 +0300

    Alfa versiune

--- Open-Terminal/Open-Terminal/FunctionChangeDirectory.hpp
+++ Open-Terminal/Open-Terminal/FunctionChangeDirectory.hpp
@@ -10,6 +10,7 @@ fs::path ChangeDirectoryKids(fs::path path_kids, fs::path path_parent) {
 	SetConsoleCP(CP_UTF8);       //
 	for (fs::path dir : fs::directory_iterator(path_parent)) {
 		if (path_kids == dir.stem()) {
+			FileList(dir);
 			return dir;
 		}
 	}
@@ -26,7 +27,7 @@ fs::path ChangeDirectory(fs::path path) {
 	SetConsoleOutputCP(CP_UTF8); // Setarea limbei română în terminal
 	SetConsoleCP(CP_UTF8);       //
 	fs::path argument;
-	std::cout << "Introduceți denumirea dirctivei";
+	std::cout << "Introduceți denumirea dirctivei: ";
 	std::cin >> argument;
 	if (argument == "..") {
 		path = ChangeDirectoryParent(path);
